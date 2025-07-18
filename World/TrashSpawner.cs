1b0fe8c9cbec044a007fe7251e375c7e45e7a135
Darrel Zhao <darrelzhao06@gmail.com>
2025-07-18 10:20:48 -0500

    changed trash area distribution + added debug mode

--- World/TrashSpawner.cs
+++ World/TrashSpawner.cs
@@ -10,8 +10,12 @@ public class TrashSpawner : MonoBehaviour
     [Header("Trash Spawner Settings")]
     //public GameObject trashPrefab; // Prefab for the trash object
     public int numberOfTrashItems = 10; // Number of trash items to spawn
-    public float roadWidth = 5f; // Width of the road area where trash can spawn
 
+    [Header("Debug Mode")]
+    public bool debugMode = false; // Enable debug mode to visualize trash spawning
+
+    float roadWidth = 0.4f; // Width of the road area where trash can spawn
+    float[] axisOffsets = new float[] { -1.6f, 1.6f }; // Possible offsets for trash spawning
     void Start()
     {
         SpawnTrash();
@@ -70,20 +74,33 @@ public class TrashSpawner : MonoBehaviour
             int randomEdgeIndex = Random.Range(0, adjacentEdges.Count);
             int toNode = adjacentEdges[randomEdgeIndex].Target;
 
+
+
             // Randomly select a position on the road using lerp()
             Vector3 fromPos = map.NodeToWorld(fromNode);
             Vector3 toPos = map.NodeToWorld(toNode);
+            Vector3 roadDirection = (toPos - fromPos).normalized;
+            fromPos += roadDirection * 2f; // offset from the start of the road
+            toPos -= roadDirection * 2f; // offset from the end of the road
 
             // Find orthogonal heading to the road
-            Vector3 roadDirection = (toPos - fromPos).normalized;
             Vector3 orthogonalDirection = new Vector3(-roadDirection.z, 0, roadDirection.x);
 
-            // Calculate a random position along the road, offset by the orthogonal direction
-            Vector3 spawnPosition = Vector3.Lerp(fromPos, toPos, Random.Range(0f, 1f));
-            Vector3 offset = orthogonalDirection * Random.Range(-roadWidth / 2f, roadWidth / 2f);
-            spawnPosition += offset;
+            // check debugMode
+            if (debugMode)
+                debugSpawnTrash(fromPos, toPos, orthogonalDirection);
 
-            // Instantiate the trash prefab at the calculated position
+            // Calculate a random position along the road, offset by the orthogonal direction
+            // Concentrate trash on sides of the roads; first randomly pick left or right side, then apply offset
+            float axisOffset = axisOffsets[Random.Range(0, axisOffsets.Length)];
+            Vector3 spawnPosition = Vector3.Lerp(fromPos, toPos, Random.Range(0f, 1f));
+            spawnPosition += orthogonalDirection * axisOffset;
+
+            // Randomly offset the spawn position on either side of the road
+            Vector3 randomOffset = orthogonalDirection * Random.Range(-roadWidth / 2f, roadWidth / 2f);
+            spawnPosition += randomOffset;
+
+            // Instantiate one of the trash prefabs at the calculated position
             int pickTrashIndex = Random.Range(0, trashArray.Length);
             GameObject trashPrefab = trashArray[pickTrashIndex];
             var trashGO = Instantiate(trashPrefab, spawnPosition, Quaternion.identity, transform);
@@ -92,5 +109,16 @@ public class TrashSpawner : MonoBehaviour
             trashGO.transform.rotation = Quaternion.Euler(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f));
             trashGO.name = $"TrashItem_{i}";
         }
+    }
+
+    public void debugSpawnTrash(Vector3 fromPos, Vector3 toPos, Vector3 orthogonalDirection)
+    {
+        // highlight the orthogonal direction for debugging
+        Debug.DrawLine(fromPos + orthogonalDirection * axisOffsets[1], toPos + orthogonalDirection * axisOffsets[1], Color.red, 20f);
+        Debug.DrawLine(fromPos - orthogonalDirection * axisOffsets[1], toPos - orthogonalDirection * axisOffsets[1], Color.red, 20f);
+        Debug.DrawLine(fromPos + orthogonalDirection * (axisOffsets[1] + roadWidth / 2f), toPos + orthogonalDirection * (axisOffsets[1] + roadWidth / 2f), Color.green, 20f);
+        Debug.DrawLine(fromPos + orthogonalDirection * (axisOffsets[1] - roadWidth / 2f), toPos + orthogonalDirection * (axisOffsets[1] - roadWidth / 2f), Color.green, 20f);
+        Debug.DrawLine(fromPos - orthogonalDirection * (axisOffsets[1] + roadWidth / 2f), toPos - orthogonalDirection * (axisOffsets[1] + roadWidth / 2f), Color.green, 20f);
+        Debug.DrawLine(fromPos - orthogonalDirection * (axisOffsets[1] - roadWidth / 2f), toPos - orthogonalDirection * (axisOffsets[1] - roadWidth / 2f), Color.green, 20f);
     }
 }
